f2b2f1de038a3f9dd7f0e126b05ba96f14678fa1
HIENLTH 2023 <hyhien@gmail.com>
2024-02-23 11:29:20 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF21BITV03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string HoTen;
+			Console.Write("Nhap ten ban: ");
+			HoTen = Console.ReadLine();
+			Console.WriteLine($"Chao ban {HoTen}");
+		}
+	}
+}
