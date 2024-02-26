7c1314b4c65e97eb9ba8d5a3ce10cd6d6cd49a7e
HIENLTH 2023 <hyhien@gmail.com>
2024-02-26 08:39:27 +0700

    Demo Bang cuu chuong

---
+++ BangCuuChuong/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+BT1: Viết hàm in bảng cửu chương của N
+Input: N
+Output: Bảng cửu chương
+	N X 1 = ?
+	...
+	N x 10 = ?
+*/
+void InBangCuuChuong(int N)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		Console.WriteLine($"{N} X {i} = {N * i}");
+	}
+}
+
+var random = new Random();
+InBangCuuChuong(random.Next(1, 100));
+
+/*BT2: Viết hàm kiểm tra 1 chuỗi có là Palindom hay không
+(chuỗi đối xứng)*/
+bool IsPalindonre(string chuoiKiemTra)
+{
+	int length = chuoiKiemTra.Length;
+	for (int i = 0; i < length / 2; i++)
+	{
+		if (chuoiKiemTra[i] != chuoiKiemTra[length - 1 - i])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool checkPalindore(string chuoi) => chuoi == chuoi.Reverse().ToString();
+bool checkPalindore2(string chuoi) { return chuoi == chuoi.Reverse().ToString(); }
