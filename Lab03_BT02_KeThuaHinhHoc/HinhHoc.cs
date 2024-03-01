054eda656ca38a7a4c81a5e02e2cc3a34ca26c72
HIENLTH 2023 <hyhien@gmail.com>
2024-03-01 12:07:10 +0700

    Quan ly hinh hoc

---
+++ Lab03_BT02_KeThuaHinhHoc/HinhHoc.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_BT02_KeThuaHinhHoc
+{
+	public class HinhHoc
+	{
+		public double DienTich { get; set; }
+		public double ChuVi { get; set; }
+		public virtual void TinhDienTichChuVi() { }
+		public override string ToString()
+		{
+			return "Hinh chua xac dinh";
+		}
+	}
+
+	public class HinhChuNhat : HinhHoc
+	{
+		public double Dai { get; set; }
+		public double Rong { get; set; }
+		public override void TinhDienTichChuVi()
+		{
+			DienTich = Dai * Rong;
+			ChuVi = (Dai + Rong) * 2;
+		}
+		public override string ToString()
+		{
+			return $"HCN D={Dai}, R={Rong}, P={ChuVi}, S={DienTich}";
+		}
+	}
+
+	public class HinhTron : HinhHoc
+	{
+		public double BanKinh { get; set; }
+		public override void TinhDienTichChuVi()
+		{
+			DienTich = BanKinh * BanKinh * Math.PI;
+			ChuVi = 2 * BanKinh * Math.PI;
+		}
+		public override string ToString()
+		{
+			return $"Tron R={BanKinh}, P={ChuVi}, S={DienTich}";
+		}
+	}
+}
---
+++ Lab03_BT02_KeThuaHinhHoc/Program.cs
@@ -0,0 +1,18 @@
+﻿using Lab03_BT02_KeThuaHinhHoc;
+
+List<HinhHoc> dsHinh = new List<HinhHoc>();
+
+//tự nhập từng loại hình
+dsHinh.Add(new HinhTron { BanKinh = 7 });
+dsHinh.Add(new HinhChuNhat { Dai = 11, Rong = 2 });
+dsHinh.Add(new HinhChuNhat { Dai = 8, Rong = 8 });
+dsHinh.Add(new HinhTron { BanKinh = 17 });
+dsHinh.Add(new HinhTron { BanKinh = 27 });
+dsHinh.Add(new HinhChuNhat { Dai = 11, Rong = 22 });
+dsHinh.Add(new HinhTron { BanKinh = 37 });
+
+foreach(var hh in dsHinh)
+{
+	hh.TinhDienTichChuVi();
+	Console.WriteLine(hh);
+}
